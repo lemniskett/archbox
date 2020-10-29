b89f5fc95c36c42edb237412071d1a08829b824c
lemniskett <lemniskett@outlook.com>
2020-10-29 22:04:39 +0700

    Do not overwrite existing config file

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 mkdir -p /usr/local/share/archbox/bin
 cp -v ./archbox.bash /usr/local/bin/archbox
-cp -v ./archbox.conf /etc/archbox.conf
+[[ ! -e /etc/archbox.conf ]] && cp -v ./archbox.conf /etc/archbox.conf
 cp -v ./copyresolv.bash /usr/local/share/archbox/bin/copyresolv
 cp -v ./archboxcommand.bash /usr/local/share/archbox/bin/archbox
 cp -v ./chroot_setup.bash /usr/local/share/archbox/chroot_setup.bash
