ed01f1bb552add10f45597e0834964e250f98cd4
lemniskett <lemniskett@outlook.com>
2020-11-02 05:12:35 +0000

    Set permission when installing.

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 mkdir -p /usr/local/share/archbox/bin
-cp -v ./archbox.bash /usr/local/bin/archbox
-[[ ! -e /etc/archbox.conf ]] && cp -v ./archbox.conf /etc/archbox.conf
-cp -v ./copyresolv.bash /usr/local/share/archbox/bin/copyresolv
-cp -v ./archboxcommand.bash /usr/local/share/archbox/bin/archbox
-cp -v ./chroot_setup.bash /usr/local/share/archbox/chroot_setup.bash
+install -v -D -m 755 ./archbox.bash /usr/local/bin/archbox
+[[ ! -e /etc/archbox.conf ]] && install -v -D -m 755 ./archbox.conf /etc/archbox.conf
+install -v -D -m 755 ./copyresolv.bash /usr/local/share/archbox/bin/copyresolv
+install -v -D -m 755 ./archboxcommand.bash /usr/local/share/archbox/bin/archbox
+install -v -D -m 755 ./chroot_setup.bash /usr/local/share/archbox/chroot_setup.bash
