aa26dba71bbda3ca3acf5256578ee573d9952282
lemniskett <lemniskett@outlook.com>
2020-11-03 22:45:26 +0700

    Change install script.

--- install.sh
+++ install.sh
@@ -8,6 +8,10 @@ install -v -D -m 755 ./archboxcommand.bash /usr/local/share/archbox/bin/archbox
 install -v -D -m 755 ./remount_run.bash /usr/local/share/archbox/bin/remount_run
 install -v -D -m 755 ./chroot_setup.bash /usr/local/share/archbox/chroot_setup.bash
 
+[[ -z $1 ]] && exit 0
+
 if [ $1 = "--exp" ]; then
 	install -v -D -m 755 ./exp/startx-killxdg.bash /usr/local/bin/startx-killxdg
+else
+	echo "Unknown install option: $1"
 fi
