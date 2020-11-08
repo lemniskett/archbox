fdcb3acf30c2119703c8bccc1f64567728cbdcc1
lemniskett <lemniskett@outlook.com>
2020-11-08 22:42:18 +0700

    Password setup failsafe

--- chroot_setup.bash
+++ chroot_setup.bash
@@ -19,7 +19,11 @@ CHROOT_USER="$(cat /tmp/archbox_user)"
 useradd -m $CHROOT_USER
 gpasswd -a $CHROOT_USER wheel
 echo "Enter root password"
-passwd
+while true; do
+	passwd && break
+done
 echo "Enter $CHROOT_USER password"
-passwd $CHROOT_USER
+while true; do
+	passwd $CHROOT_USER && break
+done
 sed -i 's/# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/g' /etc/sudoers
