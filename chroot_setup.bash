32fea4d2803153810f31cae314711b672f10df75
lemniskett <lemniskett@outlook.com>
2020-11-10 00:02:27 +0700

    Add locale and timezone setup

--- chroot_setup.bash
+++ chroot_setup.bash
@@ -13,7 +13,7 @@ msg "Initializing pacman keyrings..."
 pacman-key --init
 pacman-key --populate archlinux
 msg "Installing essential packages..."
-pacman -Syu base base-devel xorg pulseaudio --noconfirm
+pacman -Syu base base-devel xorg pulseaudio nano --noconfirm
 msg "Installing servicectl..."
 mkdir -p /usr/local/share/servicectl/enabled
 curl -L 'https://raw.githubusercontent.com/lemniskett/servicectl/master/servicectl' > /usr/local/share/servicectl/servicectl 2>/dev/null
@@ -21,6 +21,20 @@ curl -L 'https://raw.githubusercontent.com/lemniskett/servicectl/master/serviced
 chmod +x /usr/local/share/servicectl/service{d,ctl}
 ln -s /usr/local/share/servicectl/servicectl /usr/local/bin/servicectl
 ln -s /usr/local/share/servicectl/serviced /usr/local/bin/serviced
+msg "Setting up locale..."
+echo "Uncomment needed locale, enter to continue"
+read
+nano /etc/locale.gen
+locale-gen
+msg "Setting up timezone..."
+echo "Enter your timezone, for example : \"Asia/Jakarta\"" 
+while true; do
+	read TIMEZONE \
+		&& [[ -e /usr/share/zoneinfo/$TIMEZONE ]] \
+		&& ln -s /etc/localtime \
+		&& break \
+		|| err "Timezone not found"
+done
 msg "Creating user account..."
 CHROOT_USER="$(cat /tmp/archbox_user)"
 useradd -m $CHROOT_USER
